40b687421601994105ceb6518cacb56ac8cc411e
Wu Tong <fengqiwu2003@foxmail.com>
2024-09-27 01:36:00 +0800

    修复启动报错

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,19 @@
 BOT_FOLDER=/amiyabot
 
 # step 1: 解压/覆盖bot文件
-tar -zxvf amiyabot.tar.gz -C $BOT_FOLDER
+if [ -f "/amiyabot.tar.gz" ]; then
+    tar -zxvf /amiyabot.tar.gz -C /
+    mv /temp/* $BOT_FOLDER
+    rm -rf /temp
+fi
 
+# step 2: 判断是否首次运行
 if [ ! -f "$BOT_FOLDER/first_run" ]; then
-    # step 2: 进入bot目录
     cd $BOT_FOLDER
-    # step 3: 初始化配置文件
     python entrypoint.py
-    # step 4: 标记已初始化
     touch first_run
 fi
 
-# step 5: 运行bot
+# step 3: 启动bot
 cd $BOT_FOLDER
 python amiya.py
